c872da984ae1fa6de2667d906bc53e3fc0850538
Ruben Uribe <uriben2000@gmail.com>
2019-08-07 16:16:14 -0500

    updated video feed to use webRTC

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ app.get('/patrol',function(req,res){
         watchId: null
     };
     madeID=false
+    const Id=makeid(8);
     while(!madeID){//makes unique Id
         const Id=makeid(8);
         if(vallidID.indexOf(Id)=== -1){
@@ -105,7 +106,7 @@ app.get('/patrol',function(req,res){
     }
     const auth=true;
     if(auth){
-        res.render('patrol',{id: Id,username=process.env.TURN_USERNAME,password=process.env.TURN_PASSWORD});
+        res.render('patrol',{id: Id,username:process.env.TURN_USERNAME,password:process.env.TURN_PASSWORD});
 
         patrol=io
         .of('/'+Id)
@@ -132,6 +133,9 @@ app.get('/patrol',function(req,res){
                 console.log("patrolID "+socketId.patrolId);
                 
             });
+            socket.on('candidate',function(candidate){
+                socket.to(socketId.watchId).emit('candidate',candidate);
+            });
             socket.on('watching',function(watchId){
                 console.log('watch connected to socket');
                 console.log(watchId);
@@ -140,11 +144,24 @@ app.get('/patrol',function(req,res){
                 socket.to(socketId.patrolId).emit('watching');
                 console.log("emited");
             });
+            socket.on('offer',function(offer){
+                console.log("recived offer from patrol");
+                //add password to database or something else that is secure
+                console.log("offer.sdp: ", offer.sdp);
+                socket.to(socketId.watchId).emit('offer',offer);
+            });
+            socket.on('answer',function(answer){
+                console.log('recived answer from watch');
+                socket.to(socketId.patrolId).emit('answer',answer);
+            });
+            socket.on('success',function(){
+                socket.to(socketId.watchId).emit('success');
+            });
             socket.on('message', function () { });
 
 
             socket.on('disconnect', function () {
-                vallidID.remove(vallidID.indexOf(Id));
+                vallidID.splice(vallidID.indexOf(Id),1);
              });            
         });
 
@@ -156,7 +173,7 @@ app.get('/patrol',function(req,res){
 app.get('/watch:videoId',function(req,res){
     const videoId=req.params.videoId;
     
-    res.render('watch',{id:videoId});
+    res.render('watch',{id:videoId,username:process.env.TURN_USERNAME,password:process.env.TURN_PASSWORD});
 });
 
 
--- public/js/watch.js
+++ public/js/watch.js
@@ -1,11 +1,45 @@
+const watch = new RTCPeerConnection(pcConfig);
+const button = document.getElementById("watch-button");
+const videoElement=document.getElementById("remote-video");
+var remoteVideo = new MediaStream();
+
+button.addEventListener('click',function(){
+    videoElement.srcObject=remoteVideo;
+});
 socket.on('connect', function () {
     console.log("watch connected");
     socket.emit('watching',socket.id);
-
+    socket.on('offer',function(offer){
+        // offerJson=JSON.parse(offer);
+        console.log(offer);
+        watch.setRemoteDescription(offer)
+        .then((offer) => {
+            watch.createAnswer(offer)
+            .then((answer)=>{
+                watch.setLocalDescription(answer);
+                socket.emit('answer',answer);
+                console.log('Sent answer');
+            })
+        });
+    });
+    watch.ontrack= function(event){
+        console.log("ontrack");
+        console.log(event);
+        console.log("recivers",watch.getReceivers());
+        remoteVideo.addTrack(event.track);
+        console.log(remoteVideo.getTracks());
+    };
     socket.on('message', function (msg) {
         console.log("in");
     });
-    socket.on('candidate',function(candidate){
-        //handle reciving ice candidates
-    })
+    socket.on('candidate',function(message){
+        console.log('candidate ', message);
+        var candidate = new RTCIceCandidate({
+            sdpMLineIndex: message.label,
+            candidate: message.candidate
+        });
+        watch.addIceCandidate(candidate);
+    });
 });
+
+  
